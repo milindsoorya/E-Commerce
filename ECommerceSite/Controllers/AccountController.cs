0185ff29bd7b17e3c2243d94fb27e7e5ce5d1881
Milind Soorya S <milind.sooryas@dxc.com>
2020-09-09 23:07:12 +0530

    Added User Registration

---
+++ ECommerceSite/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECommerceSite;
+
+namespace ECommerceSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult AddorEdit(int id = 0)
+        {
+            tbluser user = new tbluser();
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult AddorEdit(tbluser user)
+        {
+            using (ECommerceEntities db = new ECommerceEntities())
+            {
+                if(db.tblusers.Any(x => x.userid == user.userid))
+                {
+                    ViewBag.DuplicateMessage = "User Name Already Exists.";
+                    return View("AddorEdit", user);
+                }
+                else
+                {
+                    db.tblusers.Add(user);
+                    db.SaveChanges();
+                }
+                
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddorEdit",new tbluser());
+        }
+    }
+}
